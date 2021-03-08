caef759a9b2c0bd107358b1f3c4b7b3ac96c82c3
MCKnaus <michael.knaus@unisg.ch>
2021-03-08 15:30:24 +0100

    Ensure that always treated and controls in folds

--- R/utils_effects.R
+++ R/utils_effects.R
@@ -26,32 +26,50 @@ prep_w_mat = function(w) {
 #'
 #' @param n Number of observations.
 #' @param cf Number of cross-fitting folds.
+#' @param w_mat Optional logical matrix of treatment indicators (n x T+1). For example created by \code{\link{prep_w_mat}}.
+#' If specified, cross-fitting folds will preserve the treatment rations from full sample.
 #' @param cl Optional vector of cluster variable if cross-fitting should account for clusters.
 #'
-#' @importFrom dplyr ntile
 #' @import stats
 #'
 #' @return Logical matrix of cross-fitting folds (n x # folds).
 #'
 #' @export
 #'
-prep_cf_mat = function(n,cf,cl=NULL) {
-
+prep_cf_mat = function(n,cf,w_mat=NULL,cl=NULL) {
   if (cf == 1) cf_mat = matrix(rep(1,n),ncol=1)
 
   if (!is.null(cl)) {
-    fold = ntile(runif(length(unique(cl))),cf)
+    rnd_id = sample(1:length(unique(cl)),length(unique(cl)))
+    fold = as.numeric(cut(rnd_id, breaks=quantile(rnd_id, probs=seq(0,1, length = cf+1)),include.lowest=TRUE))
     fold = factor(fold[match(cl,unique(cl))])
-    cf_mat = model.matrix(~0+fold)
+    cf_mat = (model.matrix(~0+fold) == 1)
   }
   else {
-    fold = factor(ntile(runif(n),cf))
-    cf_mat = model.matrix(~0+fold)
+    if (is.null(w_mat)) {
+      rnd_id = sample(1:n,n)
+      fold = factor(as.numeric(cut(rnd_id, breaks=quantile(rnd_id, probs=seq(0,1, length = cf+1)),include.lowest=TRUE)))
+      cf_mat = (model.matrix(~0+fold) == 1)
+    }
+    else {
+      cf_mat = matrix(NA,n,cf)
+      nw = colSums(w_mat)
+      for (i in 1:ncol(w_mat)) {
+        cf_mat_w = matrix(FALSE,nw[i],cf)
+        rnd_id = sample(1:nw[i],nw[i])
+        fold = as.numeric(cut(rnd_id, breaks=quantile(rnd_id, probs=seq(0,1, length = cf+1)),include.lowest=TRUE))
+        for (j in 1:cf) {
+          cf_mat_w[fold == j,j] = TRUE
+        }
+        cf_mat[w_mat[,i],] = cf_mat_w
+      }
+    }
   }
   colnames(cf_mat) = sprintf("CF %d",1:cf)
 
-  return(cf_mat == 1)
+  return(cf_mat)
 }
+
 
 
 #' Runs classification analysis (CLAN) for CATEs.
