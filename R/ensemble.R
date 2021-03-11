64775d713ca52ac74a8a7402f18435a8f64e7198
MCKnaus <michael.knaus@unisg.ch>
2021-03-11 18:35:49 +0100

    Fix case of collinear predictors in nnls

--- R/ensemble.R
+++ R/ensemble.R
@@ -54,6 +54,8 @@ ensemble = function(ml,
 
     # Calculate weights each method receives
     nnls_weights = nnls::nnls(fit_cv,y)$x
+    # In case of perfectly agreeing predictions nnls provides only zeros
+    if (sum(nnls_weights) == 0) nnls_weights = nnls_weights + 1 / ncol(nnls_weights)
     nnls_weights = nnls_weights / sum(nnls_weights)
 
     # Run all methods on the full sample
