a350311d3da5cad7e0a899e6a1aeab9a9b004828
MCKnaus <michael.knaus@unisg.ch>
2021-11-21 13:30:15 +0100

    Remove psych

--- R/data_prep_utils.R
+++ R/data_prep_utils.R
@@ -1,5 +1,5 @@
 #' Create interactions and polynomials
-#' 
+#'
 #' This function takes a dataset and strings with variable names as input and creates interactions and polynomials
 #'
 #' @param data Matrix with the main variables.
@@ -14,18 +14,18 @@
 #' @export
 #'
 design_matrix = function(data,int=NULL,int_d=2,poly=NULL,poly_d=2,log=NULL) {
-  
+
   # Part for interactions
   if (!is.null(int)) {
     int = paste0("+(",paste0(int,collapse="+"),")^",toString(int_d))
   }
-  
+
   # Part for polynomials
   if (!is.null(poly)) {
     poly = paste0("+poly(",paste0(poly,collapse=paste0(",",toString(poly_d),",raw=TRUE)+poly(")),",",toString(poly_d),",raw=TRUE)",
                    "-(",paste0(paste0(poly,collapse="+")),")")
   }
-  
+
   # Part for logs
   if (!is.null(log)) {
     # Check whether some variables can't be logged because not positive
@@ -36,10 +36,10 @@ design_matrix = function(data,int=NULL,int_d=2,poly=NULL,poly_d=2,log=NULL) {
     }
     log = paste0("+log(",paste0(log,collapse=")+log("),")")
   }
-  
+
   # Combine the three parts
   fmla = as.formula(paste("~0",int,poly,log))
-  
+
   # Generate matrix
   data = model.matrix(fmla,data=as.data.frame(data))
   # Clean variable names to make sense
@@ -47,13 +47,13 @@ design_matrix = function(data,int=NULL,int_d=2,poly=NULL,poly_d=2,log=NULL) {
   colnames(data) = gsub(paste0(", ",toString(poly_d),", raw = TRUE)"),"",colnames(data))
   colnames(data) = gsub("log\\(","ln_",colnames(data))
   colnames(data) = gsub("\\)","",colnames(data))
-  
+
   return(data)
 }
 
 
 #' Data screening
-#' 
+#'
 #' This function takes a matrix of data and removes
 #' 1. Variables without variation
 #' 2. Dummy variables where one group is nearly empty (optional in one of both treatment groups)
@@ -67,23 +67,22 @@ design_matrix = function(data,int=NULL,int_d=2,poly=NULL,poly_d=2,log=NULL) {
 #'
 #' @return Screened matrix
 #'
-#' @import matrixStats psych
+#' @import matrixStats
 #'
 #' @export
 #'
 data_screen = function(data,treat=NULL,bin_cut=0.01,corr_cut=0.99,print=FALSE) {
-  
+
   ## Kick out variables with no variation
   # Identify the names
   nm_del = colnames(data)[colSds(data) == 0]
   # Describe identified variables
   if (print==TRUE) {
     cat("\n\n Variables with no variation:",nm_del,"\n\n")
-    if (identical(nm_del, character(0)) == FALSE) print(describe(data[,nm_del],fast=TRUE))
   }
   # Remove identified variables
   if (identical(nm_del, character(0)) == FALSE) data = data[,!colnames(data) %in% nm_del]
-  
+
   ## Remove dummy variables lower than threshold in one of the two treatment groups
   # Identify dummies
   bin = apply(data,2,function(x) { all(x %in% 0:1) })
@@ -97,30 +96,28 @@ data_screen = function(data,treat=NULL,bin_cut=0.01,corr_cut=0.99,print=FALSE) {
     mean0 = colMeans(data[d==0,])
     bel_cut = (mean1<bin_cut | mean1 > (1-bin_cut) | mean0<bin_cut | mean0 > (1-bin_cut))
   }
-  
+
   # Identify names that are binary and close to 0 and 1
   nm_del = colnames(data)[bin & bel_cut]
   if (print==TRUE) {
     cat("\n\n Dummy variables close to 0 or 1:",nm_del,"\n\n")
-    if (identical(nm_del, character(0)) == FALSE) print(describe(data[,nm_del],fast=TRUE))
   }
-  
+
   # Remove identified variables
   if (identical(nm_del, character(0)) == FALSE) data = data[,!colnames(data) %in% nm_del]
-  
+
   ## Remove all redundant (nearly perfectly correlated) variables
   # Calculate correlation matrix and consider only upper diagonal
   cor = (abs(cor(data))>corr_cut)
   cor[lower.tri(cor, diag=TRUE)] = FALSE
-  
+
   # Identify names of redundant variables
   nm_del = colnames(cor)[colSums(cor)>0]
-  
+
   if (print==TRUE) {
     cat("\n\n Variables (nearly) perfectly correlated:",nm_del,"\n\n")
-    if (identical(nm_del, character(0)) == FALSE) print(describe(data[,nm_del],fast=TRUE))
   }
   if (identical(nm_del, character(0)) == FALSE) data = data[,colSums(cor)==0]
-  
+
   return(data)
 }
